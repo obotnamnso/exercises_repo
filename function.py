da6c769f0c42d4c637fec93ae2b24b46cd0fb87c
obotnamnso <benniiobot@gmail.com>
2019-02-12 18:51:19 +0100

    Some intro functions in function.py

---
+++ function.py
@@ -0,0 +1,15 @@
+# Homework Assignment #2: Functions
+
+# Function 1
+
+def Artist():
+	print('Artist : Benni Obot')
+	return
+Artist()
+
+# Function 2
+
+def Genre():
+	print('GEnre : Blues')
+	return
+Genre()
